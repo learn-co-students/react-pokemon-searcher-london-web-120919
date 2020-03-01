6c246716cb005801a0959400575ac07d6aeb8e76
Mariola Pasko <mariolapasko@Mariolas-MacBook-Air.local>
2020-03-01 22:33:53 +0000

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,39 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      switch: true
+    }
+  }
+
+  toggleImage = () => {
+    this.setState({
+      switch: !this.state.switch
+    })
+  }
+
+
   render() {
+    // console.log(this.props.stats)
+    const {img, name, backImg, stats} = this.props
     return (
       <Card>
-        <div>
+        <div onClick={() => this.toggleImage()}>
           <div className="image">
-            <img alt="oh no!" />
+            <img alt="oh no!" src={this.state.switch ? img : backImg} />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              hp: {
+                stats.find(stat => stat.name === 'hp').value
+              }
             </span>
           </div>
         </div>
