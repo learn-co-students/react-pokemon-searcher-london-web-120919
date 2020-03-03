b04297ee79978b2e4d1cfbbdf38d8e8c3cd834da
AdShEaTaKoRe <tomy.berman1@gmail.com>
2020-03-03 00:00:58 +0000

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,20 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+
+
+renderAllPokemon = (pokemons) => {
+  return pokemons.map(pokemon =>( <PokemonCard pokemon={pokemon} key={pokemon.id} />))
+}
+
+
   render() {
+    
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+
+        {this.renderAllPokemon(this.props.pokemons)}
+
       </Card.Group>
     )
   }
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,17 +1,41 @@
-import React from 'react'
-import { Form } from 'semantic-ui-react'
+import React from "react";
+import { Form } from "semantic-ui-react";
 
 class PokemonForm extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
-      name: '',
-      hp: '',
-      frontUrl: '',
-      backUrl: ''
-    }
+      name: "",
+      hp: "",
+      frontUrl: "",
+      backUrl: ""
+    };
   }
+
+  handleSubmit = e => {
+    const newPokemon = {
+      name: e.target.name.value,
+      stats: [{ name: "hp", value: e.target.hp.value }],
+      sprites: {
+        front: e.target.frontUrl.value,
+        back: e.target.backUrl.value
+      }
+    };
+    this.setState(newPokemon);
+    fetch("http://localhost:3000/pokemon", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newPokemon)
+    })
+      .then(response => response.json())
+      .then(json => this.props.newPokemon(json))
+    e.target.reset()
+    e.preventDefault();
+  };
 
   render() {
     return (
@@ -21,14 +45,24 @@ class PokemonForm extends React.Component {
           <Form.Group widths="equal">
             <Form.Input fluid label="Name" placeholder="Name" name="name" />
             <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input
+              fluid
+              label="Front Image URL"
+              placeholder="url"
+              name="frontUrl"
+            />
+            <Form.Input
+              fluid
+              label="Back Image URL"
+              placeholder="url"
+              name="backUrl"
+            />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
       </div>
-    )
+    );
   }
 }
 
-export default PokemonForm
+export default PokemonForm;
